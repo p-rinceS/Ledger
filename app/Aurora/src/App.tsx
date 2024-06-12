73800b920389e0721b8a87551c79fd1c57dea1f7
PrinceS <princesonani9112@gmail.com>
2024-06-11 21:03:08 -0500

    added initialization of project
    added idocs system and included react with vite build, utilizing yarn.

---
+++ app/Aurora/src/App.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+       Test
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ app/Aurora/src/components/NavigationBar.tsx
@@ -0,0 +1,15 @@
+import {FC} from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+const NavigationBar: FC = () => {
+  return (
+
+    <div>
+        Some Nav Bar, Huh?
+    </div>
+  );
+};
+
+export default NavigationBar;
