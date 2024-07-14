69e57d2cc210c3783c9e4b5ad13b30149248385e
PrinceS <princesonani9112@gmail.com>
2024-07-13 23:33:12 -0500

    refactor: Update IGDBFetcher to use client_bearer_access_token class

--- app/Aegis/app/src/main/java/org/example/app/IGDBFetcher.java
+++ app/Aegis/app/src/main/java/org/example/app/IGDBFetcher.java
@@ -14,30 +14,32 @@ import java.net.http.HttpResponse;
 import org.example.secrets.client_secret;
 import org.example.secrets.client_bearer_access_token;
 
-public class Authentication {
+public class IGDBFetcher {
 
     private static final String CLIENT_ID = "yaneluq4z6l6jqaew0q4av423ch6t5";
     private static final String CLIENT_SECRET = client_secret.getCLIENT_SECRET();
+    private static client_bearer_access_token token = new client_bearer_access_token();
 
     public static void GET_api_request() throws IOException, InterruptedException {
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.igdb.com/v4/games"))
+                .uri(URI.create("https://api.igdb.com/v4/games/"))
                 .header("Client-ID", CLIENT_ID)
-                .header("Authorization", client_bearer_access_token.getClientBearerAccessToken())
+                .header("Authorization", token.getClientBearerAccessToken())
                 .header("Accept", "application/json")
+                // POST
+                .POST(HttpRequest.BodyPublishers.ofString(
+                        "fields name; limit 500;"))
                 .build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response.statusCode());
         System.out.println(response.body());
     }
 
     public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
 
-        // %s is a placeholder for the client_id and client_secret
         URI authUri = new URI(String.format(
                 "https://id.twitch.tv/oauth2/token?client_id=%s&client_secret=%s&grant_type=client_credentials",
                 CLIENT_ID, CLIENT_SECRET));
@@ -46,11 +48,9 @@ public class Authentication {
         HttpURLConnection http = (HttpURLConnection) con;
         http.setRequestMethod("POST");
         http.setDoOutput(true);
-        // 200 GOOD
-        System.out.println(http.getResponseCode());
-        GET_api_request();
 
-        // print response Body
+        System.out.println(http.getResponseCode());
+
         BufferedReader in = new BufferedReader(new InputStreamReader(http.getInputStream()));
         String inputLine;
         StringBuffer content = new StringBuffer();
@@ -58,10 +58,14 @@ public class Authentication {
             content.append(inputLine);
         }
         in.close();
-        System.out.println(content);
 
-        // print type of content
-        System.out.println(http.getContentType());
+        String access_token = content.substring(content.indexOf("access_token") + 15,
+                content.indexOf("expires_in") - 3);
+        System.out.println(access_token);
+
+        token.setclientBearerAccessToken(access_token);
+
+        GET_api_request();
 
     }
 
